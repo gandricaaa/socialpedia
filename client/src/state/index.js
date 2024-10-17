e5b926d88e316dc20e65e15c77297bf12d8d5a6e
gandricaaa <draganblagojevic558@gmail.com>
2024-10-17 19:46:07 +0200

    Finished client side

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,24 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = {
     mode: "light",
     user: null,
     token: null,
     posts: [],
-}
+};
+
 export const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
         setMode: (state) => {
-            state.mode = state.mode === "light" ? "dark" : "light"
+            state.mode = state.mode === "light" ? "dark" : "light";
         },
         setLogin: (state, action) => {
             state.user = action.payload.user;
             state.token = action.payload.token;
         },
         setLogout: (state) => {
-            state.user = null
-            state.token = null
+            state.user = null;
+            state.token = null;
         },
         setFriends: (state, action) => {
             if (state.user) {
@@ -32,12 +34,14 @@ export const authSlice = createSlice({
         },
         setPost: (state, action) => {
             const updatedPosts = state.posts.map((post) => {
-                if (post._id === action.payload.post_id) return action.payload.post
+                if (post._id === action.payload.post._id) return action.payload.post;
                 return post;
-            })
-            state.posts = updatedPosts
-        }
-    }
-})
-export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions
-export default authSlice.reducer
+            });
+            state.posts = updatedPosts;
+        },
+    },
+});
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+    authSlice.actions;
+export default authSlice.reducer;
