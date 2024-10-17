1fb4fcf93be6896c4161acdb3535d635f442dbde
gandricaaa <draganblagojevic558@gmail.com>
2024-10-17 18:24:20 +0200

    Created Components folder created pages, and createt AuthSlice and state folder for global instances

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
+import HomePage from "./scenes/homePage/homePage";
+import LoginPage from "./scenes/loginPage/loginPage";
+import ProfilePage from "./scenes/profilePage/ProfilePage";
+
 
 function App() {
+
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
